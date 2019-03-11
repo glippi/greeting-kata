25d8f87f3241ae2fd00cd42afdabe94b6c8e0ea0
Gabriele Lippi <gabriele@lippi.net>
2019-03-11 12:37:25 +0100

    Implement code fro 4th requirement and small refactor

--- src/main/java/Greeting.java
+++ src/main/java/Greeting.java
@@ -1,14 +1,14 @@
 import java.util.List;
 
-public class Greeting {
-    public static String greet(String name) {
+class Greeting {
+    static String greet(String name) {
         if (name == null) return "Hello, my friend.";
         if(isUppercaseName(name))
             return "HELLO " + name + "!";
         return "Hello, Bob.";
     }
 
-    public static String greet(List<String> names) {
+    static String greet(List<String> names) {
         return "Hello, " + names.get(0) + " and " + names.get(1) + ".";
     }
 
