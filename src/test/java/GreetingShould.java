ff31ca4c4d779f59bbb0643ce50b7c1a360db330
Gabriele Lippi <gabriele@lippi.net>
2019-03-11 11:52:39 +0100

    Refactor test code

--- src/test/java/GreetingShould.java
+++ src/test/java/GreetingShould.java
@@ -1,18 +1,17 @@
-import org.junit.Before;
 import org.junit.Test;
-import static org.junit.Assert.assertThat;
+
 import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
 
 public class GreetingShould {
-    private Greeting greeting;
 
     @Test
     public void greet_bob(){
-        assertThat(greeting.greet("Bob"), is("Hello, Bob."));
+        assertThat(Greeting.greet("Bob"), is("Hello, Bob."));
     }
 
     @Test
     public void greet_hello_friend_when_name_is_null() {
-        assertThat(greeting.greet(null), is("Hello, my friend."));
+        assertThat(Greeting.greet(null), is("Hello, my friend."));
     }
 }
