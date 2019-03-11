69981193f45d7ca30cf69f2216fafe89ec8bf2ca
Alasdair Smith <alasdairsmith0491@gmail.com>
2019-03-11 11:25:56 +0000

    Add test for multiple names

--- src/main/java/Greeting.java
+++ src/main/java/Greeting.java
@@ -1,9 +1,15 @@
+import java.util.List;
+
 public class Greeting {
     public static String greet(String name) {
         if (name == null) return "Hello, my friend.";
         if(isUppercaseName(name))
             return "HELLO " + name + "!";
         return "Hello, Bob.";
+    }
+
+    public static String greet(List<String> names) {
+        return null;
     }
 
     private static boolean isUppercaseName(String name) {
--- src/test/java/GreetingShould.java
+++ src/test/java/GreetingShould.java
@@ -1,5 +1,6 @@
 import org.junit.Test;
 
+import static java.util.Arrays.asList;
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
 
@@ -12,11 +13,16 @@ public class GreetingShould {
 
     @Test
     public void greet_hello_friend_when_name_is_null() {
-        assertThat(Greeting.greet(null), is("Hello, my friend."));
+        assertThat(Greeting.greet((String) null), is("Hello, my friend."));
     }
 
     @Test
     public void shout_out_JERRY() {
         assertThat(Greeting.greet("JERRY"), is("HELLO JERRY!"));
+    }
+
+    @Test
+    public void greet_two_people_for_a_list_of_names() {
+        assertThat(Greeting.greet(asList("Jill", "Jane")), is("Hello, Jill and Jane."));
     }
 }
