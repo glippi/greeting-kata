bcb6c5c3c9612bd1adabe0332ebf8b752efffb0b
Alasdair Smith <alasdairsmith0491@gmail.com>
2019-03-11 10:59:15 +0000

    Implement shout greeting.

--- src/main/java/Greeting.java
+++ src/main/java/Greeting.java
@@ -1,6 +1,8 @@
 public class Greeting {
     public static String greet(String name) {
         if (name == null) return "Hello, my friend.";
+        if(name.toUpperCase().equals(name))
+            return "HELLO " + name.toUpperCase() + "!";
         return "Hello, Bob.";
     }
 }
