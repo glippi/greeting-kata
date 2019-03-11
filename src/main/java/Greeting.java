849aef9e2fa8f2503c796d515e42d48d9277ad70
Alasdair Smith <alasdairsmith0491@gmail.com>
2019-03-11 11:49:25 +0000

    Implement code for requirement 5.

--- src/main/java/Greeting.java
+++ src/main/java/Greeting.java
@@ -9,7 +9,19 @@ class Greeting {
     }
 
     static String greet(List<String> names) {
-        return "Hello, " + names.get(0) + " and " + names.get(1) + ".";
+        StringBuilder greetingMultiple = new StringBuilder();
+        if(names.size() == 2)
+            return "Hello, " + names.get(0) + " and " + names.get(1) + ".";
+
+        for (String name : names) {
+            if (names.indexOf(name) == names.size() - 1) {
+                greetingMultiple.append("and " + name + ".");
+                break;
+            }
+            greetingMultiple.append(name + ", ");
+        }
+
+        return "Hello, " + greetingMultiple.toString();
     }
 
     private static boolean isUppercaseName(String name) {
